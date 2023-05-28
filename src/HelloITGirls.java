aa0450ffd25dd5bc5f9262e6d69b01bec4e631f3
wikuskina <wikuskina@gmail.com>
2023-05-28 05:20:14 +0100

    New commit

---
+++ src/HelloITGirls.java
@@ -0,0 +1,8 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class HelloITGirls {
+    public static void main(String[] args) {
+        System.out.printf("Hello IT Girls!");
+
+    }
+}
