19a9822881569e352e279f08ad8e21852ce2635d
wikuskina <wikuskina@gmail.com>
2023-06-03 02:15:25 +0100

    Finished Week 2 homework

--- src/HelloITGirls.java
+++ src/HelloITGirls.java
@@ -1,8 +1,91 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.time.*;
 public class HelloITGirls {
     public static void main(String[] args) {
-        System.out.printf("Hello IT Girls!");
+//        String x0 = null; // empty string
+//        String x = "Hello It Girls!"; // new string
+//        String x2 = new String("Hi It Girls!"); // new string 2
+//
+//        byte a = 1;
+//        short b = 30000;
+//        int c = 488;
+//        long d = -2L;
+//        float e = 1.5F;
+//        double f =299.9F ;
+//        boolean isDone = true ;
+//        char g = 'A'; // character A example
+//        char g2 = 0x0041; // character A in Unicode
+//        char g3 = 65; // character A in ASCII values
+
+//       Boxing, autoboxing and unboxing integer
+//        int intBox = 9;
+//        Integer newIntBox = new Integer(intBox); // boxing
+//        System.out.println(newIntBox);
+//
+//        int intBox1 = 56;
+//        Integer newIntBox1 = intBox1; // Auto Boxing
+//        System.out.println(newIntBox1);
+//
+//        Integer intBox2 = 45;
+//        int newIntBox2 = intBox2; // auto UnBoxing;
+//        System.out.println(newIntBox2);
+
+//       // Boxing, autoboxing and unboxing byte
+//        char c = 'g';
+//        Character newC = new Character(c); // boxing
+//        System.out.println(newC);
+//
+//        char b = '1';
+//        Character newB = b; // auto Boxing
+//        System.out.println(newB);
+//
+//        Character a = 'f';
+//        char newA = a ; // auto UnBoxing;
+//        System.out.println(newA);
+
+
+
+//        printing out results
+//        System.out.println(x0);
+//        System.out.println(x);
+//        System.out.println(x2);
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        System.out.println(d);
+//        System.out.println(e);
+//        System.out.println(f);
+//        System.out.println(g);
+//        System.out.println(g2);
+//        System.out.println(g3);
+//        System.out.println(isDone);
+
+        //  площадь прямоугольного треугольника равна половине произведения его катетов.
+        // S = 1/2 (a × b), где a и b — катеты
+//        int a = 5;
+//        int b = 8;
+//        int sq = ((a*b)/2); // площадь
+//        System.out.println(sq);
+
+        // По теореме Пифагора найдём гипотенузу
+        //int c = (Math. sqrt((Math. pow(2,a) + (Math. pow(2,b))); - ERROR?
+        //System.out.println(c);
+
+//        int b = 50;
+//        int c = 20;
+//        System.out.println(++b >= 51);
+//        System.out.println(--c < 22);
+
+        String training = "Training Java";
+        System.out.println(training.length()); // length of string
+        System.out.println(training.charAt(2)); // character at 2
+
+
+// import java.time.*; create LocalDate and print today's date
+        LocalDate today = LocalDate.now();
+        System.out.println(today);
+
 
     }
 }
