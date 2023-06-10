382c998de2d944a51f5970c5f69041e8a33feaa8
wikuskina <wikuskina@gmail.com>
2023-06-10 21:40:17 +0100

    Carpark - class Car - final - homework 3

--- src/Car.java
+++ src/Car.java
@@ -1,25 +1,73 @@
+//public class Car {
+//
+//    // Name of the car
+//    String name;
+//
+//    // Colour of the car
+//    String colour;
+//
+//    // Car goes
+////    public String go(){
+////        return " goes down the road";
+//
+//    }
+//    // Car slows down
+////    public String slow(){
+////        return " and slows down";
+//    }
+//
+//    public Car(String name, String colour){
+//        this.name = name;
+//        this.colour = colour;
+//    }
+//
+//    public static void main (String[] args) {
+//        Car audi = new Car ("Audi", "Blue");
+//        Car lexus = new Car ("Lexus", "White");
+//        Car bentley = new Car ("Bentley", "Black");
+//
+////        System.out.println("This car is "+ audi.name + " and its colour is " + audi.colour);
+////        System.out.println("This car is "+ lexus.name + " and its colour is " + lexus.colour);
+////        System.out.println("This car is "+ bentley.name + " and its colour is " + bentley.colour);
+////        System.out.println(bentley.name + bentley.go() + bentley.slow());
+////        System.out.println(lexus.name + bentley.go() + bentley.slow());
+////        System.out.println(audi.name + bentley.go() + bentley.slow());
+//    }
+//
+//
+//
+//}
+
 public class Car {
 
-    // Name of the car
     String name;
-
-    // Colour of the car
     String colour;
 
-    public Car(String name, String colour){
-        this.name = name;
-        this.colour = colour;
-    }
+    int year = 0;
+
+    // CONSTRUCTOR 2 - WITH NO ARGUMENTS
 
     public static void main (String[] args) {
-        Car audi = new Car ("Audi", "Blue");
-        Car lexus = new Car ("Lexus", "White");
-        Car bentley = new Car ("Bentley", "Black");
+        Car audi = new Car ();
+        audi.name = "Audi";
+        audi.colour = "Blue";
+        audi.year = 1999;
 
-        System.out.println("This car is "+ audi.name + " and its colour is " + audi.colour);
-        System.out.println("This car is "+ lexus.name + " and its colour is " + lexus.colour);
-        System.out.println("This car is "+ bentley.name + " and its colour is " + bentley.colour);
-    }
+        Car lexus = new Car ();
+        lexus.name = "Lexus";
+        lexus.colour = "White";
+
+        Car bentley = new Car ();
+        bentley.name = "Bentley";
+        bentley.year = 2021;
+
+        System.out.println("This car is "+ audi.name + " its colour is " + audi.colour + " and its year is " + audi.year);
+        System.out.println("This car is "+ lexus.name + " its colour is " + lexus.colour);
+        System.out.println("This car is "+ bentley.name + " and its year is " + bentley.year);
+        }
+
+
+
 
 
 
